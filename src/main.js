499fcc6313254f9e997c4f89344ef73c14aaa015
dengyi <dengyi@dengyi.pro>
2020-07-23 23:50:10 +0800

    login and logout

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import './plugins/element.js'
 import Axios from 'axios'
 
 Axios.defaults.baseURL = 'http://127.0.0.1:8888'
+Axios.interceptors.request.use(config => {
+  console.log('拦截器中封装token')
+  config.headers.token = window.sessionStorage.getItem('token')
+  return config
+})
 
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios
